92877f0615b45d778df80f3bfca0c5f679f35b19
pushan maharjan <pusanmaharjan16@gmail.com>
2025-11-15 11:50:46 +0545

    www.pushan-maharjan.com.np

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import Feauture from './components/Feature/Feauture'
-import TechExperties from './components/Feature/TechExperties'
-import WorkflowSteps from './components/How-we-work/WorkflowSteps'
-import Hero2 from './components/Hero/Hero2'
-import Project from './components/Project/Project'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Hero2 from './components/Hero/Hero2';
+import Feauture from './components/Feature/Feauture';
+import TechExperties from './components/Feature/TechExperties';
+import WorkflowSteps from './components/How-we-work/WorkflowSteps';
+import Project from './components/Project/Project';
 
 const App = () => {
   return (
-    <>
-       <Navbar />
-       <Hero />
-       <Hero2 />
-       <Feauture />
-       <TechExperties />
-       <Project />
-       <WorkflowSteps />
-    </>
-  )
+    <BrowserRouter basename="/pushan-portfolio">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <Hero2 />
+            <Feauture />
+            <TechExperties />
+            <Project />
+            <WorkflowSteps />
+          </>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
